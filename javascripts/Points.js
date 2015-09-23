51cd7473014c3522216bea5efbd3780a8329d6b1
NCPS <nimblewillproperties@gmail.com>
2015-09-23 17:32:29 -0400

    Create Points.js

---
+++ javascripts/Points.js
@@ -0,0 +1,71 @@
+Var points={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.57893371582031,
+          33.96500329452543
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.5068359375,
+          33.98379436669471
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.54048156738281,
+          34.027624047624265
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.59541320800781,
+          34.022502265437836
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.60983276367186,
+          33.93139678750913
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.53155517578124,
+          33.95076486932678
+        ]
+      }
+    }
+  ]
+}
